e2a9fef2536609b5000fc7029aa9e92dd29742ca
angelaroggero <36828779+angelaroggero@users.noreply.github.com>
2018-02-26 12:54:19 -0500

    Added local repository

---
+++ assets/js/functions.js
@@ -0,0 +1,99 @@
+$(document).ready(function() {
+
+
+
+
+    $(".link_cincinnati").on("mouseover", function() {
+        $(".img_cincinnati").show()
+    }).on("mouseout", function() {
+        $(".img_cincinnati").hide()
+    }), 
+
+
+
+
+    $(".link_angela").on("mouseover", function() {
+        $(".img_angela").show()
+    }).on("mouseout", function() {
+        $(".img_angela").hide()
+    }),
+
+
+
+    $(".link_uc").on("mouseover", function() {
+        $(".img_uc").show()
+    }).on("mouseout", function() {
+        $(".img_uc").hide()
+    }),
+
+
+
+
+        $(".link_trine").on("mouseover", function() {
+        $(".img_trine").show()
+    }).on("mouseout", function() {
+        $(".img_trine").hide()
+    }),
+
+
+
+    $(".link_golf").on("mouseover", function() {
+        $(".img_golf").show()
+    }).on("mouseout", function() {
+        $(".img_golf").hide()
+    }),
+
+
+
+
+     $(".link_cat").on("mouseover", function() {
+        $(".img_cat").show()
+    }).on("mouseout", function() {
+        $(".img_cat").hide()
+    }),
+
+
+
+
+     $(".link_tv").on("mouseover", function() {
+        $(".img_tv").show()
+    }).on("mouseout", function() {
+        $(".img_tv").hide()
+    }),
+
+
+
+
+     $(".link_travel").on("mouseover", function() {
+        $(".img_travel").show()
+    }).on("mouseout", function() {
+        $(".img_travel").hide()
+    }),
+
+
+
+
+     $(".link_hnu").on("mouseover", function() {
+        $(".img_hnu").show()
+    }).on("mouseout", function() {
+        $(".img_hnu").hide()
+    }),
+
+
+
+
+     $(".link_colorado").on("mouseover", function() {
+        $(".img_colorado").show()
+    }).on("mouseout", function() {
+        $(".img_colorado").hide()
+    }),
+
+
+
+
+     $(".link_china").on("mouseover", function() {
+        $(".img_china").show()
+    }).on("mouseout", function() {
+        $(".img_china").hide()
+    })
+});
